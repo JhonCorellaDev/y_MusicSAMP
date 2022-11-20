d21499c06c7357bcb6a1cf263429256683a80046
Jhonatan Javier <74806674+eltiojhon7@users.noreply.github.com>
2022-11-20 17:01:51 -0300

    Update index.php

--- index.php
+++ index.php
@@ -6,8 +6,8 @@
 		Con este sencillo script podras facilmente buscar canciones por su nombre y se reproduciran en tu Servidor
 
 */
-$nome = (isset($_GET['n'])) ? urlencode($_GET['n']) : exit("Escribe un Nombre");
-$url = "https://api.yourdomain.com.sa/sounds/?s=".$nome;
+$name = (isset($_GET['n'])) ? urlencode($_GET['n']) : exit("Escribe un Nombre");
+$url = "https://api.yourdomain.com.sa/sounds/?s=".$name;
 $content = file_get_contents($url);
 $first_step = explode( '<audio controls preload="none">' , $content );
 $links = array();
